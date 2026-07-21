7e39798736b966c373e3325968a8d47f8c6dd61e
endrys7 <kashuby7@gmail.com>
2026-07-22 00:27:06 +0200

    Create texto.js

---
+++ texto.js
@@ -0,0 +1,51 @@
+const mensajes=[
+
+{
+texto:"CANJEA EL CODIGO",
+tiempo:3000,
+color:"#00d4ff"
+},
+
+{
+texto:"KASHUBY-LATAM",
+tiempo:5000,
+color:"#00ff88"
+}
+
+];
+
+let actual=0;
+
+const div=document.getElementById("mensaje");
+
+function mostrar(){
+
+div.style.opacity=0;
+
+setTimeout(()=>{
+
+div.innerHTML=mensajes[actual].texto;
+
+div.style.color=mensajes[actual].color;
+
+div.style.opacity=1;
+
+setTimeout(()=>{
+
+actual++;
+
+if(actual>=mensajes.length){
+
+actual=0;
+
+}
+
+mostrar();
+
+},mensajes[actual].tiempo);
+
+},300);
+
+}
+
+mostrar();
