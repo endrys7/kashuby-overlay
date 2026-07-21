7f02d36a419605e2f7fb758b027b023ce27e09ed
endrys7 <kashuby7@gmail.com>
2026-07-22 00:17:26 +0200

    Create text.js

---
+++ text.js
@@ -0,0 +1,7 @@
+const titulo="KASHUBY-LATAM";
+
+const anuncio="10K PARACOINS";
+
+document.getElementById("textoPrincipal").innerHTML=titulo;
+
+document.getElementById("textoSecundario").innerHTML=anuncio;
