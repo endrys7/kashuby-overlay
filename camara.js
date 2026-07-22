51167de926a36fc20802699cdc15b6f2820083c1
endrys7 <kashuby7@gmail.com>
2026-07-23 00:23:19 +0200

    Update camara.js

--- camara.js
+++ camara.js
@@ -1,3 +1,18 @@
-const url="https://vdo.ninja/?view=RngAaQJ";
+const url = "https://vdo.ninja/?view=CgNEpKZ";
 
-document.getElementById("camara").src=url;
+/**************
+CONFIGURACIÓN
+**************/
+
+const zoom = 1.35;
+
+const moverX = -12;
+
+const moverY = -6;
+
+document.getElementById("camara").src = url;
+
+const cam = document.getElementById("camara");
+
+cam.style.transform =
+`translate(${moverX}%,${moverY}%) scale(${zoom})`;
